a36d12a3b5f1ecb98ab3bfac040f0d1c7deb83ec
unknown <eduard.teixeira@etec.sp.gov.br>
2021-08-05 18:35:25 -0300

    modularizando codigo

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-class Cliente {
-    nome;
-    cpf;
-    rg;
-}
-
-class ContaCorrente {
-    agencia;
-    #saldo = 1000;
-    // https://github.com/tc39/proposal-class-fields#private-fields
-
-    sacar(valor) {
-        if (this.#saldo >= valor) {
-            this.#saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor) {
-        if (valor <= 0) {
-            return;
-        }
-        this.#saldo += valor;
-    }
-}
+import { Cliente } from './Cliente.js';
+import { ContaCorrente } from './ContaCorrente.js';
 
 const client1 = new Cliente();
 client1.nome = 'Nome 1';
@@ -31,13 +8,9 @@ client1.rg = 1234567;
 
 const conta1 = new ContaCorrente();
 conta1.agencia = 1234;
-// conta1.sacar(500);
-// conta1.depositar(500);
+conta1.cliente = client1;
 
-const valorSaque = conta1.sacar(100);
-console.log('Valor saque ::: ' + valorSaque)
 
-console.log(client1, conta1);
 
 const client2 = new Cliente();
 client2.nome = 'Nome 2';
@@ -45,5 +18,14 @@ client2.cpf = 12345678955;
 
 const conta2 = new ContaCorrente();
 conta2.agencia = 4567;
+conta2.cliente = client2;
 
-console.log(client2, conta2);
+
+
+conta1.depositar(1000.33);
+
+conta1.transferir(250, conta2);
+
+console.log(conta1, conta1.verSaldo());
+
+console.log(conta2, conta2.verSaldo());
