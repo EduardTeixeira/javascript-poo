eefb7713c10fecd4377a7d0e067baa669d30a67e
unknown <eduard.teixeira@etec.sp.gov.br>
2021-08-05 16:39:15 -0300

    adicionando comportamentos

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
     rg;
+}
+
+class ContaCorrente {
+    agencia;
+    #saldo = 1000;
+    // https://github.com/tc39/proposal-class-fields#private-fields
+
+    sacar(valor) {
+        if (this.#saldo >= valor) {
+            this.#saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor) {
+        if (valor <= 0) {
+            return;
+        }
+        this.#saldo += valor;
+    }
 }
 
 const client1 = new Cliente();
 client1.nome = 'Nome 1';
 client1.cpf = 12345678900;
-client1.agencia = 1234;
-client1.saldo = 100.50;
 client1.rg = 1234567;
+
+const conta1 = new ContaCorrente();
+conta1.agencia = 1234;
+// conta1.sacar(500);
+// conta1.depositar(500);
+
+const valorSaque = conta1.sacar(100);
+console.log('Valor saque ::: ' + valorSaque)
+
+console.log(client1, conta1);
 
 const client2 = new Cliente();
 client2.nome = 'Nome 2';
 client2.cpf = 12345678955;
-client2.agencia = 4567;
-client2.saldo = 23506.57;
 
-console.log(client1, client2);
+const conta2 = new ContaCorrente();
+conta2.agencia = 4567;
+
+console.log(client2, conta2);
